08786118b7eb553b913c02898678ca3d5671eb3a
sav <aevs.andres.vidal@gmail.com>
2024-03-19 01:44:36 -0600

    new attributes added

--- src/models/CustomerModel.java
+++ src/models/CustomerModel.java
@@ -1,16 +1,23 @@
 package models;
 
+import java.math.BigDecimal;
+
 public class CustomerModel {
     private String name;
     private String lastname;
     private String dni;
     private String curp;
+    private BigDecimal pkContact;
+    private BigDecimal pkDirection;
 
-    public CustomerModel(String name, String lastname, String dni, String curp) {
+    public CustomerModel(String name, String lastname, String dni, String curp, BigDecimal pkContact,
+            BigDecimal pkDirection) {
         this.name = name;
         this.lastname = lastname;
         this.dni = dni;
         this.curp = curp;
+        this.pkContact = pkContact;
+        this.pkDirection = pkDirection;
     }
 
     public String getName() {
@@ -43,6 +50,22 @@ public class CustomerModel {
 
     public void setCurp(String curp) {
         this.curp = curp;
+    }
+
+    public BigDecimal getPkContact() {
+        return pkContact;
+    }
+
+    public void setPkContact(BigDecimal pkContact) {
+        this.pkContact = pkContact;
+    }
+
+    public BigDecimal getPkDirection() {
+        return pkDirection;
+    }
+
+    public void setPkDirection(BigDecimal pkDirection) {
+        this.pkDirection = pkDirection;
     }
 
 }
