e9361a32c28cfd703cf06bb3fc38a725c3f2eb71
sav <aevs.andres.vidal@gmail.com>
2024-03-20 16:58:52 -0600

    product controller added

---
+++ src/controllers/ProductController.java
@@ -0,0 +1,101 @@
+package controllers;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import config.MariaDB;
+import helpers.Key;
+import models.ProductModel;
+
+public class ProductController extends ProductModel implements Key {
+    private MariaDB mdb = new MariaDB();
+
+    public ProductController(String name, String size, double price, BigDecimal stockKey) {
+        super(name, size, price, stockKey);
+    }
+
+    public BigDecimal create() {
+        try {
+            Connection connection = mdb.connect();
+            PreparedStatement statement = connection
+                    .prepareStatement("INSERT INTO products (name, size, price, stock_id_stock) VALUES(?, ?, ?, ?)");
+            statement.setString(1, this.getName());
+            statement.setString(2, this.getSize());
+            statement.setDouble(3, this.getPrice());
+            statement.setString(4, String.valueOf(this.getStockKey()));
+            statement.executeUpdate();
+
+            BigDecimal key = generatedKey(statement);
+
+            statement.close();
+            connection.close();
+
+            return key;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public ResultSet read() {
+        try {
+            Connection connection = mdb.connect();
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM products");
+            ResultSet rs = statement.executeQuery();
+
+            statement.close();
+            connection.close();
+
+            return rs;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public boolean update(BigDecimal key) {
+        try {
+            Connection connection = mdb.connect();
+            PreparedStatement statement = connection.prepareStatement(
+                    "UPDATE products SET name = ?, size = ?, price = ?, stock_id_stock = ? WHERE pk_product = ?");
+            statement.setString(1, this.getName());
+            statement.setString(2, this.getSize());
+            statement.setString(3, String.valueOf(this.getPrice()));
+            statement.setString(4, String.valueOf(this.getStockKey()));
+            statement.setString(5, String.valueOf(key));
+            statement.executeUpdate();
+
+            statement.close();
+            connection.close();
+
+            return true;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean delete() {
+        return false;
+    }
+
+    @Override
+    public BigDecimal generatedKey(PreparedStatement statement) {
+        ResultSet rs;
+        BigDecimal key;
+        try {
+            rs = statement.getGeneratedKeys();
+            if (rs.next()) {
+                key = rs.getBigDecimal(1);
+                rs.close();
+                return key;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+}
