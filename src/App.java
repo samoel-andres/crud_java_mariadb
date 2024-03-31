6f0f10577efece59d42c25ba15abe02978dafbd1
sav <aevs.andres.vidal@gmail.com>
2024-03-31 00:10:30 -0600

    aggregate view instance

--- src/App.java
+++ src/App.java
@@ -1,5 +1,46 @@
+
+import view.HomeView;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        HomeView hv = new HomeView();
+        hv.setVisible(true);
+
+        // Controller c = new Controller(
+        // "9999",
+        // "sample@mail.com",
+        // "test",
+        // "16",
+        // "17",
+        // "test",
+        // "test",
+        // "test",
+        // "test",
+        // "test",
+        // "test",
+        // "test",
+        // "test",
+        // "10",
+        // "caja",
+        // "6",
+        // "",
+        // "3",
+        // "yogurth",
+        // "chico",
+        // "12.5",
+        // "1",
+        // "3.6",
+        // "153.6586",
+        // "50",
+        // "solicitado",
+        // "",
+        // "",
+        // "5",
+        // "6",
+        // "2024-03-25",
+        // "descuento",
+        // "15",
+        // "asdasd5");
+
     }
 }
