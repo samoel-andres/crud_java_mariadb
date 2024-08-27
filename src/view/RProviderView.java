548c0b4e826d35429915c9dcc61df10e1a2bc638
sav <aevs.andres.vidal@gmail.com>
2024-08-26 19:07:40 -0600

    read view created for provider

---
+++ src/view/RProviderView.java
@@ -0,0 +1,38 @@
+package view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JDialog;
+
+public class RProviderView extends JDialog implements ActionListener, KeyListener, FocusListener {
+
+    @Override
+    public void focusGained(FocusEvent e) {
+    }
+
+    @Override
+    public void focusLost(FocusEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+}
